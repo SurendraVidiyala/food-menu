8d9bd50e24d1efe08518d8d28cdf5c45d89b260b
surendra_vidiyala <vidiyala.surendra@gmail.com>
2017-06-19 13:57:53 -0500

    added store, helpers

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -5,11 +5,14 @@ import hero from './hero';
 import menu from './menu';
 import bottom from './bottom';
 
-export default function app(items) {
-    const modalEle = modal(items);
+//export default function app(items) {
+export default function app(store) {
+    const modalEle = modal(store);
+    //const modalEle = modal(items);
     const navbarEle = navbar();
     const heroEle = hero();
-    const menuEle = menu(items);
+    //const menuEle = menu(items);
+    const menuEle = menu(store);
     const bottomEle = bottom();
     const appEle = addId(div(modalEle, navbarEle, heroEle, menuEle, bottomEle), 'app-container');
     return appEle;
--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ const uppers = lowers.map(name => name.toUpperCase());
 uppers.forEach(name => console.log(`Hello, ${name}`)); */
 import app from './components/app';
 import { createStore } from './state';
-
+/*
 function reducer(state, event, data) {
     switch (event) {
         case 'FOO':
@@ -16,9 +16,22 @@ function reducer(state, event, data) {
             return state;
     }
 }
-
+*/
+function reducer(state, event, data) {
+    switch (event) {
+        case 'SET_ITEMS':
+            return Object.assign({}, state, {
+                items: data.items.reduce((total, item) =>
+                    Object.assign({}, total, {
+                        [item.id]: item
+                    }), {}),
+            });
+        default:
+            return state;
+    }
+}
 const store = createStore(reducer);
-
+/*
 store.on('FOO', state => {
     console.log('in FOO callback');
     console.log('state', state);
@@ -40,10 +53,12 @@ store.trigger('BAR', {
 });
 store.trigger('FOO');
 store.trigger('NOT_REAL');
-
+*/
 fetch('food.json')
     .then(res => res.json())
     .then(resBody => {
         const body = document.querySelector('body');
-        body.insertBefore(app(resBody), body.childNodes[0]);
+        // body.insertBefore(app(resBody), body.childNodes[0]);
+        body.insertBefore(app(store), body.childNodes[0]);
+        store.trigger('SET_ITEMS', { items: resBody });
     });
