09a5bc64b4f046494547f7d856b0e925f8e626d3
surendra_vidiyala <vidiyala.surendra@gmail.com>
2017-06-19 13:32:49 -0500

    created state and triggers

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,45 @@
 /* const lowers = ['alex', 'george', 'bonnie', 'maria', 'ellie'];
 const uppers = lowers.map(name => name.toUpperCase());
 uppers.forEach(name => console.log(`Hello, ${name}`)); */
-
 import app from './components/app';
+import { createStore } from './state';
+
+function reducer(state, event, data) {
+    switch (event) {
+        case 'FOO':
+            return Object.assign({}, state, {
+                name: 'suren',
+            });
+        case 'BAR':
+            return Object.assign({}, state, data);
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+store.on('FOO', state => {
+    console.log('in FOO callback');
+    console.log('state', state);
+});
+
+store.on('BAR', state => {
+    console.log('in BAR callback');
+    console.log('state', state);
+});
+
+store.on('NOT_REAL', state => {
+    console.log('in NOT_REAL callback');
+    console.log('state', state);
+});
+
+store.trigger('BAR', {
+    name: 'surya',
+    job: 'full stack web developer',
+});
+store.trigger('FOO');
+store.trigger('NOT_REAL');
 
 fetch('food.json')
     .then(res => res.json())
