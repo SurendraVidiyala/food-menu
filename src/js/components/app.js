5916a97991ec00bae75781f1fe9ad29d178113ef
surendra_vidiyala <vidiyala.surendra@gmail.com>
2017-06-19 13:01:30 -0500

    created modals

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,16 +1,16 @@
 import { div, addId } from '../builders';
+import modal from './modal';
 import navbar from './navbar';
 import hero from './hero';
 import menu from './menu';
 import bottom from './bottom';
 
 export default function app(items) {
+    const modalEle = modal(items);
     const navbarEle = navbar();
     const heroEle = hero();
     const menuEle = menu(items);
     const bottomEle = bottom();
-    const appEle = addId(div(navbarEle, heroEle, menuEle, bottomEle), 'app-container');
-
-
+    const appEle = addId(div(modalEle, navbarEle, heroEle, menuEle, bottomEle), 'app-container');
     return appEle;
 }
